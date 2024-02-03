b71dbb68ae110114092251c6df6c7c2630d1a6ff
Maria Matveeva <marikadenisova@yandex.ru>
2024-02-03 11:12:46 +0300

    mesto full first

--- frontend/src/utils/Auth.js
+++ frontend/src/utils/Auth.js
@@ -26,10 +26,16 @@ export const loginUser = (email, password) => {
   return fetch(`${BASE_URL}/signin`, {
     method: "POST",
     headers: {
+      "Accept": "application/json",
       "Content-Type": "application/json",
     },
     body: JSON.stringify({ "email":email, "password":password }),
-  }).then(handleResponse);
+  }).then(handleResponse)
+   .then((res) => {
+    localStorage.setItem('jwt', res.token)
+    return res
+   }
+   )
 };
 
 export const validateToken = (JWT) => {
